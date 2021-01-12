294897f073a16be3046683638ff985cbe3d900a7
ItsMeSKKYZ3R <71552277+ItsMeSKKYZ3R@users.noreply.github.com>
2021-01-12 17:01:38 +0100

    Update error.js

--- util/error.js
+++ util/error.js
@@ -1,11 +1,11 @@
 const { MessageEmbed } = require("discord.js");
 
 /**
- * Easy to send errors because im lazy to do the same things :p
- * @param {String} text - Message which is need to send
- * @param {TextChannel} channel - A Channel to send error
+ * @param {String} texte
+ * @param {TextChannel} channel
  */
-module.exports = async (text, channel) => {
+
+module.exports = async (texte, channel) => {
     let embed = new MessageEmbed()
         .setColor("RED")
         .setDescription(text)
